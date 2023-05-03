a7414d02113a627e804757317992c3cb58fb720f
fahmi-aldairi <fahmialdairi10@gmail.com>
2023-05-03 22:26:47 +0300

    update

--- tourism-in-jordan/src/Components/Tours.js
+++ tourism-in-jordan/src/Components/Tours.js
@@ -29,11 +29,51 @@ const arr = [
     image:
       "https://images.pling.com/img/00/00/54/70/32/1331633/debc46a923efb3f23a0043af76b6e87188c8.jpg",
   },
+  {
+    id: 1,
+    name: "Petra",
+    info: "some information about petra",
+    image:
+      "https://images.pexels.com/photos/1631665/pexels-photo-1631665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 10,
+  },
+  {
+    id: 2,
+    name: "Wadi rum",
+    info: "some information about rum",
+    image:
+      "https://images.pexels.com/photos/13458330/pexels-photo-13458330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 12,
+  },
+  {
+    id: 3,
+    name: "Jerash",
+    info: "some information about Jerash",
+    image:
+      "https://images.pexels.com/photos/4216244/pexels-photo-4216244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 6,
+  },
+  {
+    id: 4,
+    name: "Madaba",
+    info: "some information about Madaba",
+    image:
+      "https://media.istockphoto.com/id/146737795/photo/madaba-map.jpg?s=1024x1024&w=is&k=20&c=7hk9LfuYqTYwIUjCCSx8QTO1Ua8yO2tyHz1IHx_jOKE=",
+    price: 8,
+  },
+  {
+    id: 5,
+    name: "Dead Sea",
+    info: "some information about Dead Sea",
+    image:
+      "https://images.pexels.com/photos/3370311/pexels-photo-3370311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 60,
+  },
 ];
 
 const Tours = () => {
   return (
-    <div className="d-flex" style={{ minHeight: "100vh" }}>
+    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
       {arr.map((ele) => {
         return (
           <div className="container">
